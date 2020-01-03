ee3fc2ffdfe0533cd0d04d6e97ed25e362719351
kpcruz <kelvinmembership@gmail.com>
2020-01-03 14:26:04 -0800

    added Update mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,20 +1,36 @@
 const Mutations = {
-  async createItem(parent, args, context, info) {
-    // TODO: Check if they are logged in
+	async createItem(parent, args, context, info) {
+		// TODO: Check if they are logged in
 
-    const item = await context.db.mutation.createItem(
-      {
-        data: {
-          ...args
-        }
-      },
-      info
-    );
+		const item = await context.db.mutation.createItem(
+			{
+				data: {
+					...args
+				}
+			},
+			info
+		);
 
-    console.log(item);
+		console.log(item);
 
-    return item;
-  }
+		return item;
+	},
+	updateItem(parent, args, ctx, info) {
+		// first take a copy of the updates
+		const updates = { ...args };
+		// remove the ID from the updates
+		delete updates.id;
+		// run the update method
+		return ctx.db.mutation.updateItem(
+			{
+				data: updates,
+				where: {
+					id: args.id
+				}
+			},
+			info
+		);
+	}
 };
 
 module.exports = Mutations;
